b01b72de5c8a35b056f6eb8bd3e8545238a9e8b7
Omid <omid.rasouli@hezardastan.net>
2023-12-20 13:28:58 +0330

    add router

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/OmidRasouli/amuse-park/server"
 	"github.com/OmidRasouli/amuse-park/statics"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	statics.Read()
 	fmt.Println("Hello there...")
+
+	router := gin.Default()
+	api := router.Group("/")
+	{
+		api.POST("/register", server.Register)
+	}
+
+	router.Run(":8080")
 }
