c4a117b40a13f518bfb41e2b5b5ea0d1573cf1e0
Omid <omid.rasouli@hezardastan.net>
2023-12-20 13:26:05 +0330

    Add Register

--- server/user.go
+++ server/user.go
@@ -3,10 +3,12 @@ package server
 import (
 	"errors"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/OmidRasouli/amuse-park/statics"
 	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gin-gonic/gin"
 )
 
 func generateToken(username string, expireTime time.Duration) (string, error) {
@@ -34,4 +36,26 @@ func validationToken(signedToken string) (string, error) {
 		return username, nil
 	}
 	return "", errors.New("Invalid token")
+}
+
+type UserAccount struct {
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
+func Register(c *gin.Context) {
+	var userAccount UserAccount
+
+	if err := c.ShouldBind(&userAccount); err != nil {
+		c.String(http.StatusBadRequest, "bad request: %v", err)
+	}
+
+	token, err := generateToken(userAccount.Username, time.Hour*100)
+	if err != nil {
+		log.Printf("this error occurred while creating jwt: %v", err)
+		c.String(http.StatusInternalServerError, "internal error occurred: %v", err)
+	}
+
+	userAccount.Token = token
+	c.JSON(http.StatusOK, userAccount)
 }
