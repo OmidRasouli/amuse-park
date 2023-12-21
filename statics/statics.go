dbec810220cab129ec76ccc182cafe35db1b18a8
Omid <omid.rasouli@hezardastan.net>
2023-12-21 15:19:45 +0330

    add new statics

--- statics/statics.go
+++ statics/statics.go
@@ -5,9 +5,23 @@ import (
 )
 
 var (
-	SecretKey string
+	SecretKey  string
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	DBSSLMode  string
+	DBTimeZone string
 )
 
 func Read() {
 	SecretKey = os.Getenv("SECRET_KEY")
+	DBHost = os.Getenv("POSTGRES_HOST")
+	DBUser = os.Getenv("POSTGRES_USER")
+	DBPassword = os.Getenv("POSTGRES_PASSWORD")
+	DBName = os.Getenv("POSTGRES_DB")
+	DBPort = os.Getenv("POSTGRES_PORT")
+	DBSSLMode = os.Getenv("SSLMODE")
+	DBTimeZone = os.Getenv("TIMEZONE")
 }
