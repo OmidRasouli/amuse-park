58c392a4584620442d71d90616803e8496acfd7a
Omid <omid.rasouli@hezardastan.net>
2023-12-21 15:08:54 +0330

    change authentication

--- server/user.go
+++ server/user.go
@@ -11,34 +11,44 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func generateToken(username string, expireTime time.Duration) (string, error) {
-	claims := jwt.MapClaims{
-		"username": username,
-		"exp":      time.Now().Add(expireTime).Unix(),
+type JWTClaims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func generateToken(userID string, expireTime time.Duration) (string, error) {
+	claims := JWTClaims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(expireTime)),
+		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err := token.SignedString([]byte(statics.SecretKey))
 	if err != nil {
-		return "", nil
+		return "", err
 	}
 	return signedToken, nil
 }
 
-func validationToken(signedToken string) (string, error) {
-	parsedToken, err := jwt.Parse(signedToken, func(token *jwt.Token) (interface{}, error) {
+func validateToken(signedToken string) (string, error) {
+	parsedToken, err := jwt.ParseWithClaims(signedToken, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(statics.SecretKey), nil
 	})
 	if err != nil {
-		return "", err
+		return "", errors.New("invalid token")
 	}
-	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
-		username := claims["username"].(string)
-		return username, nil
+
+	claims, ok := parsedToken.Claims.(*JWTClaims)
+	if !ok || !parsedToken.Valid {
+		return "", errors.New("invalid token")
 	}
-	return "", errors.New("Invalid token")
+
+	return claims.UserID, nil
 }
 
 type UserAccount struct {
+	UserID   string `json:"user_id"`
 	Username string `json:"username"`
 	Token    string `json:"token"`
 }
@@ -46,14 +56,16 @@ type UserAccount struct {
 func Register(c *gin.Context) {
 	var userAccount UserAccount
 
-	if err := c.ShouldBind(&userAccount); err != nil {
+	if err := c.ShouldBindJSON(&userAccount); err != nil {
 		c.String(http.StatusBadRequest, "bad request: %v", err)
+		return
 	}
 
-	token, err := generateToken(userAccount.Username, time.Hour*100)
+	token, err := generateToken(userAccount.UserID, time.Hour*100)
 	if err != nil {
 		log.Printf("this error occurred while creating jwt: %v", err)
 		c.String(http.StatusInternalServerError, "internal error occurred: %v", err)
+		return
 	}
 
 	userAccount.Token = token
