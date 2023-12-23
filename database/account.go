10d504ee08c4f06342ca24376758780db66da7d6
Omid <omid.rasouli@hezardastan.net>
2023-12-23 12:31:11 +0330

    Use database interface

--- database/account.go
+++ database/account.go
@@ -2,8 +2,16 @@ package database
 
 import "github.com/OmidRasouli/amuse-park/models"
 
-func CreateAccount(account *models.Account) error {
-	result := DB.Create(account)
+func (r *RealDatabaseHandler) CreateAccount(account *models.Account) error {
+	result := r.DB.Create(account)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *RealDatabaseHandler) CreateAuthentication(authentication *models.Authentication) error {
+	result := r.DB.Create(authentication)
 	if result.Error != nil {
 		return result.Error
 	}
