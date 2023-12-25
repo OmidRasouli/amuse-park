fce503c932275214c2a8e1b4f75a170f45a6adce
Omid <omid.rasouli@hezardastan.net>
2023-12-25 18:28:16 +0330

    add regex handler

---
+++ common/regex_handler.go
@@ -0,0 +1,12 @@
+package common
+
+import (
+	"regexp"
+)
+
+func IsValidEmail(email string) bool {
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	regex := regexp.MustCompile(emailRegex)
+
+	return len(regex.FindStringIndex(email)) != 0
+}
